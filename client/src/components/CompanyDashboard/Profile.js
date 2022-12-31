3f56f5c6f96ebd9290c9a09460d0afd4707aee1e
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-12-31 16:53:13 +0300

    created the profile component
    added the post,view survey buttons more styling to be added later

--- client/src/components/CompanyDashboard/Profile.js
+++ client/src/components/CompanyDashboard/Profile.js
@@ -1,19 +1,45 @@
 import React from "react";
+import '../../styles/CompanyDashboard/profile.css'
 
 function Profile() {
     
-    <div id='profile'>
-        <h1> Dashboard</h1>
 
-        <div>
+    return (
+    <div id="profile" className="container">
+      <h1> Dashboard</h1>
 
-            
+      <div id="profile-content">
+                <div className="profile-info">
+                <div>
+         </div>            
+          <img
+            className="profile-pic"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlL5FOU8xopXZ_gCoWAzrqwr_odPOKoA5Rvw&usqp=CAU"
+            alt="profile-pic"
+          />
         </div>
-    
-        
-</div>
+
+                <div>
+                    <p className="company-name">Lencare Merchants</p>
+                    <p className="company-email">lencaremerchants@gmail.com</p>
+                    <p className="company-since">
+                    Member Since  
+                        <span className="start-date"> 2022-05-22</span>
+                    </p>
+                    <button className="btn">View Profile</button>
+                </div>        
 
 
+
+        <div className="buttons">
+          <button className="btn"> Post jobs</button>
+          <button className="btn"> Post Survey</button>
+          <button className="btn"> View Survey</button>
+        </div>
+      </div>
+    </div>
+
+    )
 
 }
 export default Profile 
